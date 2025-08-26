5a811450da54a567e2062ac7b6b6e594de7efbc5
Rakhib Khan <hanser0110@proton.me>
2025-08-26 09:22:36 +0530

    Delete Downloads directory

--- Downloads/blackout-lock-gate-v4/service-worker.js
+++
@@ -1,6 +0,0 @@
-// SW v4 (cache-busted)
-const CACHE='blackout-lock-v4';
-const ASSETS=['./','./index.html','./styles.css','./lock.js?v=4','./manifest.webmanifest','./blackoutv1.9.2.html','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
