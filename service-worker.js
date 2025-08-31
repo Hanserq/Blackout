44ee9774e7897222779e04f97d050c83bf2e2d35
Rakhib <hanser0110@proton.me>
2025-08-31 22:42:37 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const VERSION = 'blackout-shell-v17';
+const VERSION = 'blackout-shell-v18';
 
 const SCOPE = (self.registration && self.registration.scope)
   ? new URL(self.registration.scope).pathname.replace(/\/$/, '')
